0f9d28cf1874cf2832f9d87c167b413928997554
douglaju <97469494+douglaju@users.noreply.github.com>
2022-03-05 21:40:30 -0500

    bug fix

--- NJCovid/global.R
+++ NJCovid/global.R
@@ -9,24 +9,18 @@ library(leaflet.extras)
 library(ggpubr)
 library(plotly)
 
-#if you create a helpers file
-#source('./helpers.R')
-
 #load csv files
 countiesCovid = read.csv(file = './countiesCovid.csv')
 countiesCurrent = read.csv(file = './countycovid312022.csv')
 countyDemo = read.csv(file = './countyDemo.csv')
 njCovid = read.csv(file = './njCovid.csv')
 
-merged_df = merge(x=counties_current, y=countyDemo, by='county', all.x=TRUE) %>%
+merged_df = merge(x=countiesCurrent, y=countyDemo, by='county', all.x=TRUE) %>%
   mutate(vaxInitiatedRatio = vaxInitiatedRatio * 100) %>%
   mutate(vaxCompletedRatio = vaxCompletedRatio * 100) %>%
   mutate(vaxAddDoseRatio = vaxAddDoseRatio * 100) %>%
   mutate(totalCasesRatio = totalCasesRatio * 1000) %>%
   mutate(totalDeathsRatio = totalDeathsRatio * 1000)
-
-#demoChoices = c(countyDemo$hsGrad, countyDemo$bachDeg, countyDemo$noInsurance,
-                #countyDemo$medianIncome, countyDemo$poverty, countyDemo$popDensity)
 
 demoChoices = names(merged_df)
 
